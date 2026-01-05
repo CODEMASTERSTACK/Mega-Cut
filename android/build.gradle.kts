9ac4790301acaca78c2af200181b2c6d6220d6c9
CODEMASTERSTACK <kanasingh974@gmail.com>
2026-01-05 20:49:32 +0530

    FIXING

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -9,6 +9,13 @@ allprojects {
         maven {
             // FFmpegKit GitHub Packages (public artifacts)
             url = uri("https://maven.pkg.github.com/tanersener/ffmpeg-kit")
+            // GitHub Packages often requires authentication. Gradle will read credentials
+            // from project properties (set in android/gradle.properties) or environment
+            // variables `GITHUB_ACTOR` and `GITHUB_TOKEN`.
+            credentials {
+                username = (providers.gradleProperty("gpr.user").orNull ?: System.getenv("GITHUB_ACTOR")) as String?
+                password = (providers.gradleProperty("gpr.key").orNull ?: System.getenv("GITHUB_TOKEN")) as String?
+            }
         }
     }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,6 @@ import FlutterMacOS
 import Foundation
 
 import audio_session
-import ffmpeg_kit_flutter_min_gpl
 import file_picker
 import just_audio
 import path_provider_foundation
@@ -15,7 +14,6 @@ import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
-  FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
